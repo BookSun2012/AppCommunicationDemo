7e6a05346dcda1fef8a985aa259c2cdfd9f95cfd
BookSun2012 <812107123@qq.com>
2016-03-30 15:52:54 +0800

    app 间的通讯 调用

---
+++ App1/App1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App1
+//
+//  Created by 周书阳 on 16/3/30.
+//  Copyright © 2016年 zhoushuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
